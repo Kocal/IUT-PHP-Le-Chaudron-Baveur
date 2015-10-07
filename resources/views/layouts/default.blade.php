8fb3e604a36acc27009110f779b503f14c4caedd
ALLIAUME Hugo <kocal@live.fr>
2015-10-07 23:44:17 +0200

    Le titre de la page est désormais échappé.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -20,7 +20,7 @@ function displayAlert() {
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>@yield('title')</title>
+    <title>{{ $__env->yieldContent('title') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     @section('css')
