cb360f11ded07f169acadcc58703bbe6a4e63fc7
ALLIAUME Hugo <kocal@live.fr>
2015-10-13 13:06:35 +0200

    Quelques modifications pour la page des enchères, et de la page d'une enchère.

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -9,7 +9,7 @@ use App\Categories;
 
 @section('content')
     <?php
-    $min_price = Bids::getBidPriceOrProductPrice($item->id);
+    $min_price = Bids::getLastBidPriceOrProductPrice($item->id);
     $min_bid = $min_price + 0.01;
     ?>
     <h2 class="item--name">{{ trim($item->name) }}</h2>
@@ -21,21 +21,29 @@ use App\Categories;
         </div>
         <div class="col-md-9">
             <div class="alert alert-info">Vendu par <b>{{ User::getEmailById($item->user_id) }}</b>,
-                dans la catégorie <b>{{ Categories::getNameById($item->category_id) }}</b>.</div>
+                dans la catégorie <b>{{ Categories::getNameById($item->category_id) }}</b>.<br>
+                Date de début de vente : <b>{{ $item->date_start }}</b>, date de fin de vente : <b>{{ $item->date_end }}</b></div>
             <p>{!! nl2br(htmlspecialchars(trim($item->description))) !!}</p>
             <hr>
             <div class="text-right">
-                <p>Prix de vente : <b>{{ $item->price }} €</b><br>
-                    Dernière enchère : <b>{{ $min_price === $item->price ? '∅' : $item->price . '€' }}</b></p>
-                    {!! BootForm::open()->class('form-inline text-right') !!}
-                    <div class="form-group {!! $errors->has('price') ? 'has-error' : '' !!}">
-                        <div class="input-group">
-                            <input type="number" name="price" id="price" class="form-control" value="<?= old('price') ?>"
-                                   placeholder="ex: {{ $min_bid  }}" step="0.01" min="{{ $min_bid }}">
-                            <div class="input-group-addon">€</div>
+                <p>
+                    Prix d'entrée : <b>{{ $item->price }} €</b><br>
+                    Dernière enchère : <b>{{ $min_price === $item->price ? '∅' : $min_price. '€' }}</b>
+                </p>
+                    {!! BootForm::open()
+                        ->action(route('bid', ['id' => $item->id]))
+                        ->class('form-inline text-right') !!}
+
+
+                        <div class="form-group  {!! $errors->has('price') ? 'has-error' : '' !!}" style="vertical-align: top">
+                            <div class="input-group">
+                                <input type="number" name="price" id="price" class="form-control" value="<?= old('price') ?>"
+                                       placeholder="ex: {{ $min_bid  }}" step="0.01" min="{{ $min_bid }}">
+                                <div class="input-group-addon">€</div>
+                            </div>
+                            {!! $errors->first('price', '<p class="help-block">:message</p>') !!}
                         </div>
-                        {!! $errors->first('price', '<p class="help-block">:message</p>') !!}
-                    </div>
+
                     {!! BootForm::submit('Enchérir')->class('btn btn-primary') !!}
                     {!! BootForm::close()  !!}
             </div>
--- resources/views/items.blade.php
+++ resources/views/items.blade.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Str;
             });
         })(jQuery);
     </script>
-
 @append
 
 @section('content')
@@ -26,7 +25,7 @@ use Illuminate\Support\Str;
     <div class="row masonry-container" >
         @foreach($items as $item)
             <?php
-                $min_price = Bids::getBidPriceOrProductPrice($item->id);
+                $min_price = Bids::getLastBidPriceOrProductPrice($item->id);
                 $min_bid = $min_price + 0.01;
             ?>
             <div class="col-md-4 col-sm-6 item">
@@ -38,19 +37,30 @@ use Illuminate\Support\Str;
                         <h4 class="item--name">
                             <a href="{{ route('item', ['id' => $item->id]) }}" title="{{ $item->name }}">{{ Str::words($item->name, 10) }}</a>
                         </h4>
-                        <p class="item--description">{!! Str::words(nl2br(htmlspecialchars($item->description)), 30) !!}</p>
                         <hr>
-                            <p>Prix de vente : <b>{{ $item->price }} €</b><br>
-                            Dernière enchère : <b>{{ $min_price === $item->price ? '∅' : $item->price . '€' }}</b></p>
+                        <p>
+                            Prix d'entrée : <b>{{ $item->price }}€</b><br>
+                            Dernière enchère : <b>{{ $min_price === $item->price ? '∅' : $min_price. '€' }}</b>
+                        </p>
                         <hr class="item--price-separator-down">
-                        {!! BootForm::open()->class('form-inline text-right') !!}
-                            <div class="form-group {!! $errors->has('price') ? 'has-error' : '' !!}">
+
+                        <?php $form_id = 'form_' . $item->id; ?>
+
+                        {!! BootForm::open()
+                                ->action(route('bid', ['id' => $item->id]))
+                                ->name('form-bid-' . $item->id)
+                                ->class('form-inline text-right') !!}
+
+                            <input type="hidden" name="_form_id" value="{{ $form_id }}">
+
+                            <div class="form-group {!! $errors->$form_id->has('price') ? 'has-error' : '' !!}">
                                 <div class="input-group">
                                     <input type="number" name="price" id="price" class="form-control" value="<?= old('price') ?>"
-                                    placeholder="ex: {{ $min_bid  }}" step="0.01" min="{{ $min_bid }}">
+                                    placeholder="ex: {{ $min_bid  }}" step="0.01" <!--min="{{ $min_bid }}-->">
                                     <div class="input-group-addon">€</div>
                                 </div>
-                                {!! $errors->first('price', '<p class="help-block">:message</p>') !!}
+
+                                {!! $errors->$form_id->first('price', '<p class="help-block">:message</p>') !!}
                             </div>
                         {!! BootForm::submit('Enchérir')->class('btn btn-primary') !!}
                         {!! BootForm::close()  !!}
@@ -64,37 +74,3 @@ use Illuminate\Support\Str;
         {!! $items->render() !!}
     </div>
 @endsection
-
-
-        <!--
-                <div class="grid-item item">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <img src="{{ asset(preg_replace('/\.jpg$/', '_thumb.jpg', $item->photo)) }}"
-                                 class="item--thumbnail" alt="">
-                        </div>
-                        <div class="col-md-9">
-                            <div class="item--title">{{ trim($item->name) }}</div>
-                            <div class="item--description">{{ Str::words(trim($item->description, 4)) }}<br><br>
-                                <b>Prix de vente: </b> {{ $item->price }}
-        </div>
-    </div>
-</div>
-<div class="item--footer">
-    <td>
-        {!! BootForm::open('bid')->class('form-inline') !!}
-
-        <div class="form-group">
-            <div class="input-group">
-                <input type="number" name="price" id="price" class="form-control input-sm" value="{{ old('price') or $min_price }} ?>"
-                                           placeholder="ex: {{ $min_price + 0.01 }}" step="0.01" min="{{ $min_price + 0.01 }}">
-                                    <div class="input-group-addon">€</div>
-                                </div>
-                            </div>
-
-                        {!! BootForm::submit('Enchérir')->class('btn btn-primary btn-sm') !!}
-{!! BootForm::close() !!}
-        </div>
-    </div>
-{{--</div>--}}
-            -->
