59bd744e69ad9c2ba66c028e19888ce8649ce0aa
ALLIAUME Hugo <kocal@live.fr>
2015-10-13 12:29:30 +0200

    - Création du Controller BidsController pour les enchères - Suppression des méthodes index(), store(), show(), edit(), update(), destroy() - Modification de la méthode create()

--- app/Http/Controllers/BidsController.php
+++ app/Http/Controllers/BidsController.php
@@ -2,85 +2,77 @@
 
 namespace App\Http\Controllers;
 
+use App\Bids;
+use App\Items;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Requests;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redirect;
 
-class BidsController extends Controller
-{
+class BidsController extends Controller {
+
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Fait quelques vérifications aux niveaux des données envoyés par l'utilisateur, puis ajoute une enchère dans le bdd
+     * @param Request $request
+     * @param $item_id
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function index()
-    {
-        //
+    public function add(Request $request, $item_id) {
+
+        $form_id = $request->input('_form_id', 'default');
+
+        // Si on est sur la page qui liste les ventes, et l'utilisateur trafique le code HTML de la page
+        if($form_id === null && preg_match('/\/items/', URL::previous())) {
+            $url_items = explode('/', $request->getPathInfo());
+            $form_id = 'form_' . array_pop($url_items);
+        }
+
+        $this->validatesRequestErrorBag = $form_id;
+
+        $item = Items::get()->where('id', $item_id)->first();
+        $min_price = Bids::getLastBidPriceOrProductPrice($item_id);
+
+        if($item === null || $min_price === null) {
+            $request->session()->flash('message', 'danger|Cette enchère n\'existe pas');
+            return redirect(route('items'));
+        }
+
+        $seller_email = User::getEmailById($item->user_id);
+
+        if(strtolower($seller_email) === strtolower(Auth::user()->email)) {
+            $request->session()->flash('message', 'danger|Il n\'est pas possible d\'enchérir votre annonce');
+            return redirect(route('items'));
+        }
+
+        $this->validate($request, [
+            'price' => 'required|numeric|min:' . $min_price
+        ]);
+
+        return $this->create($request, $item_id);
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param int $item_id
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function create()
-    {
-        //
-    }
+    public function create(Request $request, $item_id) {
+        $datas = $request->all();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        $bid = new Bids();
+        $bid->user_id = Auth::user()->id;
+        $bid->item_id = $item_id;
+        $bid->price = trim($datas['price']);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        if($bid->save()) {
+            $request->session()->flash('message', 'success|Votre enchère a bien été saisie !');
+        } else {
+            $request->session()->flash('message', 'error|Il s\'est passé un truc, dsl');
+        }
+//        return redirect(route('item', ['id' => $item_id]));
+        return redirect(URL::previous());
     }
 }
