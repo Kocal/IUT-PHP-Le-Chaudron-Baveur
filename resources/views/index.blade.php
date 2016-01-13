4fe51ebf3df1591ed095a2e00aa89bc5925cc4f0
ALLIAUME Hugo <kocal@live.fr>
2016-01-13 16:26:23 +0100

    Ajout de marges sous les h1,.h1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,22 +6,10 @@
     <h1 class="text-center">Bienvenue au Chaudron Baveur !</h1>
 
     <div class="text-justify">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate eaque eius in incidunt labore, maxime minima
-            natus possimus repellat similique, suscipit voluptatem. Delectus deleniti dignissimos dolor laboriosam nemo
-            quaerat, sed similique veniam? Adipisci alias blanditiis culpa debitis delectus dolore eius eligendi, esse fugit
-            impedit incidunt inventore ipsa, iste nam nostrum officia quaerat quas recusandae saepe soluta tempora tempore
-            voluptates voluptatum. Accusamus ad at commodi dicta earum error esse illo inventore ipsa iusto non quos, sint
-            tempora velit voluptatibus! Aliquam aperiam consequuntur dolorum enim eveniet ex expedita, molestiae neque nulla
-            numquam optio, repellat repudiandae sint soluta suscipit temporibus voluptatibus. Accusantium, aspernatur.</p>
-        <p>Aperiam architecto commodi cupiditate dolorem doloribus exercitationem id laudantium magnam modi mollitia
-            necessitatibus officiis perspiciatis, praesentium sed sit veritatis vero vitae voluptate voluptatem voluptates.
-            Blanditiis consectetur ea eaque eius fugit maiores, minus officia, omnis placeat quam quidem, voluptas? Dolor
-            earum et eum fugit placeat, sapiente sed voluptates! Aperiam autem consectetur cum delectus deleniti deserunt
-            distinctio dolore doloremque doloribus, eius eos facilis inventore, ipsum libero maiores minima non obcaecati
-            perferendis placeat quaerat quas repellat repellendus reprehenderit similique tempora tenetur unde voluptatem!
-            Earum eos necessitatibus nulla pariatur ratione rerum saepe tenetur voluptate? Aliquam amet culpa dolores,
-            expedita odio quidem quos repellendus ullam.</p>
-        <hr>
+	<p>Bienvenue sur le site du <b>Chaudron Baveur</b>, site de ventes aux enchères <i>fictif</i> créé lors d'un TP en PHP à l'IUT.</p>
+	<p>La suite romanesque fantasy <b>Harry Potter</b> narre les aventures d'un apprenti sorcier nommé Harry Potter et de ses amis Ron Weasley et Hermione Granger à l'école de sorcellerie Poudlard, dirigée par Albus Dumbledore. L'intrigue principale de la série met en scène le combat du jeune Harry Potter contre un mage noir réputé invincible, Lord Voldemort, qui a tué autrefois ses parents ; à la tête d'un clan de mages noirs, les Mangemorts, Voldemort cherche depuis des décennies à prendre le pouvoir sur le monde des sorciers.</p>
+
+	<hr>
         <h2 class="text-center">Plus grosse vente</h2>
         <p class="text-center">
             @if($maxSale === null)
